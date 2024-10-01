9043495fc21198945808ec5fd80685b8df84ce75
Crosslywere <ogboruj@outlook.com>
2024-10-01 13:17:59 +0100

    created user service

---
+++ src/main/java/com/test/security/service/UserService.java
@@ -0,0 +1,9 @@
+package com.test.security.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+
+
+}
---
+++ src/main/java/com/test/security/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.test.security.service.impl;
+
+import com.test.security.repository.UserRepository;
+import com.test.security.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username + " does not exist!"));
+    }
+
+}
